7953dcf1e1c97325fe2b32b391233a01fafe950a
Guilherme Wunderlich <guilhermewunderlich@hotmail.com>
2020-11-11 12:55:24 -0300

    .

---
+++ Twitter.py
@@ -0,0 +1,25 @@
+import tweepy
+import time
+import tokens
+
+auth = tweepy.OAuthHandler(tokens.api_key,tokens.api_secret)
+auth.set_access_token(tokens.access_token,tokens.secret_token)
+
+api=tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+
+user=api.me()
+
+
+search = 'careca'
+numeroDeTweets = 100
+
+for tweet in tweepy.Cursor(api.search, search).items(numeroDeTweets):
+    try:
+        print('tweet Retuitado e favoritado')
+        tweet.retweet()
+        tweet.favorite()
+        time.sleep(600)
+    except tweepy.TweepError as e:
+        print(e.reason)
+    except StopIteration:
+        break
